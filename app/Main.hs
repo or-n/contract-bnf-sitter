9a913db2ec66f1db39d1dc21989a8b22b85e16d1
or-n <kulombowie@gmail.com>
2025-04-13 13:19:53 +0200

    import ErrM only once

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,17 @@
 module Main where
 
+import ErrM
+
 import qualified AbsLBNF as LBNF
 import qualified ParLBNF as LBNF
-import qualified ErrM as LBNF
 import qualified PrintLBNF as LBNF
 
 import qualified AbsTreeSitter as TreeSitter
 import qualified ParTreeSitter as TreeSitter
-import qualified ErrM as TreeSitter
 import qualified PrintTreeSitter as TreeSitter
 
 import qualified AbsRustRegex as RustRegex
 import qualified ParRustRegex as RustRegex
-import qualified ErrM as RustRegex
 import qualified PrintRustRegex as RustRegex
 
 import Translate
@@ -20,7 +19,7 @@ import Translate
 main = do
   input <- readFile "samplesRustRegex/quote"
   case RustRegex.pGrammar (RustRegex.myLexer input) of
-    RustRegex.Ok tree -> do
+    Ok tree -> do
       putStrLn "Parsed successfully!"
       putStrLn (RustRegex.printTree tree)
-    RustRegex.Bad err -> putStrLn $ "Parse error:\n" ++ err
+    Bad err -> putStrLn $ "Parse error:\n" ++ err
--- app/Translate.hs
+++ app/Translate.hs
@@ -1,18 +1,17 @@
 module Translate where
 
+import ErrM
+
 import qualified AbsLBNF as LBNF
 import qualified ParLBNF as LBNF
-import qualified ErrM as LBNF
 import qualified PrintLBNF as LBNF
 
 import qualified AbsTreeSitter as TreeSitter
 import qualified ParTreeSitter as TreeSitter
-import qualified ErrM as TreeSitter
 import qualified PrintTreeSitter as TreeSitter
 
 import qualified AbsRustRegex as RustRegex
 import qualified ParRustRegex as RustRegex
-import qualified ErrM as RustRegex
 import qualified PrintRustRegex as RustRegex
 
 translate :: LBNF.Grammar -> TreeSitter.Grammar
