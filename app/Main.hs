1d0bdabbfe9f33ef58850e039cd360b9561bfcda
or-n <kulombowie@gmail.com>
2025-04-13 13:12:20 +0200

    qualified imports

--- app/Main.hs
+++ app/Main.hs
@@ -1,24 +1,24 @@
 module Main where
 
--- import AbsLBNF
--- import ParLBNF
--- import ErrM
--- import PrintLBNF
+import qualified AbsLBNF as LBNF
+import qualified ParLBNF as LBNF
+import qualified ErrM as LBNF
+import qualified PrintLBNF as LBNF
 
--- import AbsTreeSitter
--- import ParTreeSitter
--- import ErrM
--- import PrintTreeSitter
+import qualified AbsTreeSitter as TreeSitter
+import qualified ParTreeSitter as TreeSitter
+import qualified ErrM as TreeSitter
+import qualified PrintTreeSitter as TreeSitter
 
-import AbsRustRegex
-import ParRustRegex
-import ErrM
-import PrintRustRegex
+import qualified AbsRustRegex as RustRegex
+import qualified ParRustRegex as RustRegex
+import qualified ErrM as RustRegex
+import qualified PrintRustRegex as RustRegex
 
 main = do
   input <- readFile "samplesRustRegex/quote"
-  case pRustRegexGrammar (myLexer input) of
-    Ok tree -> do
+  case RustRegex.pRustRegexGrammar (RustRegex.myLexer input) of
+    RustRegex.Ok tree -> do
       putStrLn "Parsed successfully!"
-      putStrLn (printTree tree)
-    Bad err -> putStrLn $ "Parse error:\n" ++ err
+      putStrLn (RustRegex.printTree tree)
+    RustRegex.Bad err -> putStrLn $ "Parse error:\n" ++ err
